1940cf9b7ae7029217dd474a0350694214bba213
vietthang7 <nguyenvietthang12092004@gmail.com>
2025-01-08 15:38:59 +0700

    .

--- app.go
+++ app.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -12,8 +14,22 @@ import (
 var db *gorm.DB
 
 func connectDB() {
-	dsn := "root:123456@tcp(localhost:3306)/tour_web?charset=utf8mb4&parseTime=True&loc=Local"
-	var err error
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	// Lấy giá trị từ biến môi trường
+	dbName := os.Getenv("DATABASE_NAME")
+	dbUser := os.Getenv("DATABASE_USERNAME")
+	dbPassword := os.Getenv("DATABASE_PASSWORD")
+	dbHost := os.Getenv("DATABASE_HOST")
+	dbPort := os.Getenv("DATABASE_PORT")
+
+	// Chuỗi DSN
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbUser, dbPassword, dbHost, dbPort, dbName)
+
+	// Kết nối Database
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Không thể kết nối với cơ sở dữ liệu: ", err)
@@ -22,6 +38,11 @@ func connectDB() {
 }
 func main() {
 	connectDB()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
@@ -29,5 +50,5 @@ func main() {
 		return c.SendString("Hello, World!") // Trả về một thông báo cho client
 	})
 
-	log.Fatal(app.Listen(":3000"))
+	log.Fatal(app.Listen(":" + port))
 }
